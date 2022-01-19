a1cd1a0ef0724c915a0cc62fea61594d3903708d
batuhan fındık <bfindik@ekofactoring.com>
2022-01-19 09:22:56 +0300

    Add project files.

---
+++ api/Controllers/ConvertController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConvertController : ControllerBase
+    {
+        [HttpPost("ConvertToJson")]
+        public IActionResult ConvertToJson(object veri)
+        {
+
+            string veri1 = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(veri.ToString()));
+
+            System.IO.File.WriteAllText(@"C:\Users\bfindik.EKOFACTORING\Desktop\dosya.json", veri1);
+
+            return Ok();
+        }
+    }
+}
