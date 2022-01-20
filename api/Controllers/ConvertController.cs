7df956d98d5490a19267c04eca073e9545895119
batu-han3428 <81229594+batu-han3428@users.noreply.github.com>
2022-01-20 13:41:51 +0300

    .

--- api/Controllers/ConvertController.cs
+++ api/Controllers/ConvertController.cs
@@ -19,14 +19,30 @@ namespace api.Controllers
 
             string veri1 = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(veri.ToString()));
 
-            if (System.IO.File.Exists("dosya yolunu gir"))
+            if (System.IO.File.Exists(@"C:\Users\bfindik.EKOFACTORING\Desktop\dosyalar\dosya.json"))
             {
-                veri2 = System.IO.File.ReadAllText("dosya yolunu gir");
+                veri2 = System.IO.File.ReadAllText(@"C:\Users\bfindik.EKOFACTORING\Desktop\dosyalar\dosya.json") + "-";
             }
 
-            System.IO.File.WriteAllText("dosya yolunu gir", veri2+"-"+veri1);
+            System.IO.File.WriteAllText(@"C:\Users\bfindik.EKOFACTORING\Desktop\dosyalar\dosya.json", veri2 + veri1);
 
             return Ok(veri1);
+        }
+
+
+        [HttpGet("DownloadJson")]
+        public IActionResult DownloadJson()
+        {
+
+            string veri = "";
+
+            if (System.IO.File.Exists(@"C:\Users\bfindik.EKOFACTORING\Desktop\dosyalar\dosya.json"))
+            {
+                veri = System.IO.File.ReadAllText(@"C:\Users\bfindik.EKOFACTORING\Desktop\dosyalar\dosya.json");
+            }
+
+           
+            return Ok(veri);
         }
     }
 }
