571d89e7327c10f1a67733c0fea585df62a28253
batu-han3428 <81229594+batu-han3428@users.noreply.github.com>
2022-01-21 14:08:47 +0300

    .

---
+++ api/Model/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Model
+{
+    public class User
+    {
+        public string mail { get; set; }
+        public string password { get; set; }
+    }
+}
